f0b9a017ce16160150ac96ff7cd657753b9b6c4e
Sara Lissaoui <saralissaoui@gmail.com>
2023-06-20 22:57:37 +0100

    payment form validation

--- frontend/src/pages/CMI.jsx
+++ frontend/src/pages/CMI.jsx
@@ -1,25 +1,21 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  Flex,
-  Grid,
-  Loader,
-  Paper,
-  Radio,
-  Select,
-  Stack,
-  Table,
-  Text,
-  TextInput,
-} from "@mantine/core";
+import { Button, Checkbox, Flex, Grid, Loader, Radio, Select, Stack, Table, Text, TextInput } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 import api from "../api/axios";
 import { useLocalStorage } from "@mantine/hooks";
 import { useNavigate } from "react-router-dom";
+import { useForm, zodResolver } from "@mantine/form";
+import { z } from "zod";
 
 const thStyle = { background: "#F5F5F5", color: "#F44336", fontWeight: "normal" };
+
+const schema = z.object({
+  name: z.string().min(2, { message: "Name invalid" }),
+  cardNumber: z.string().length(16),
+  expirationMonth: z.string().min(1, "Month invalid"),
+  expirationYear: z.string().min(1, "Year invalid"),
+  verificationCode: z.string().length(3, "Verification code invalid"),
+  toc: z.literal(true),
+});
 
 function CMI() {
   const navigate = useNavigate();
@@ -28,6 +24,17 @@ function CMI() {
   const [user, setUser] = useLocalStorage({ key: "user" });
   const [loading, setLoading] = useState(false);
   const [paymentLoading, setPaymentLoading] = useState(false);
+  const paymentForm = useForm({
+    initialValues: {
+      name: "",
+      cardNumber: "",
+      expirationMonth: "",
+      expirationYear: "",
+      verificationCode: "",
+      toc: false,
+    },
+    validate: zodResolver(schema),
+  });
 
   const fetchCart = async () => {
     setLoading(true);
@@ -94,23 +101,47 @@ function CMI() {
                             style={{ marginLeft: 18 }}
                           />
                         </Text>
-
-                        <Radio label="Credit card" defaultChecked />
-                        <TextInput label="Name on card" placeholder="NAME ON CARD" />
-                        <TextInput label="Card number" placeholder="CARD NUMBER" />
-                        <Flex>
-                          <Select
-                            label="Expiration date"
-                            data={Array.from(Array(12).keys()).map((month) => ({
-                              value: month + 1,
-                              label: `${month + 1}`.padStart(2, "0"),
-                            }))}
-                            value={1}
+                        <form onSubmit={paymentForm.onSubmit(handleCreateOrder)} id="paymentForm">
+                          <Radio label="Credit card" defaultChecked />
+                          <TextInput
+                            label="Name on card"
+                            placeholder="NAME ON CARD"
+                            mt={20}
+                            {...paymentForm.getInputProps("name")}
                           />
-                          <Select ml={12} label=" " data={[{ value: 2023, label: 2023 }]} value={2023} />
-                        </Flex>
-                        <TextInput label="Verification code" placeholder="" w={250} />
-                        <Checkbox label="I agree to the terms and conditions" mb={20} />
+                          <TextInput
+                            label="Card number"
+                            placeholder="CARD NUMBER"
+                            {...paymentForm.getInputProps("cardNumber")}
+                          />
+                          <Flex>
+                            <Select
+                              label="Expiration date"
+                              data={Array.from(Array(12).keys()).map((month) => ({
+                                value: `${month + 1}`,
+                                label: `${month + 1}`.padStart(2, "0"),
+                              }))}
+                              {...paymentForm.getInputProps("expirationMonth")}
+                            />
+                            <Select
+                              ml={12}
+                              label=" "
+                              data={[{ value: "2023", label: "2023" }]}
+                              {...paymentForm.getInputProps("expirationYear")}
+                            />
+                          </Flex>
+                          <TextInput
+                            label="Verification code"
+                            placeholder=""
+                            w={250}
+                            {...paymentForm.getInputProps("verificationCode")}
+                          />
+                          <Checkbox
+                            label="I agree to the terms and conditions"
+                            my={20}
+                            {...paymentForm.getInputProps("toc", { type: "checkbox" })}
+                          />
+                        </form>
                       </Stack>
                     </td>
                   </tr>
@@ -187,10 +218,16 @@ function CMI() {
                 </Table>
 
                 <Flex gap={12}>
-                  <Button fullWidth color="green" onClick={handleCreateOrder} disabled={paymentLoading}>
+                  <Button
+                    fullWidth
+                    type="submit"
+                    color="green"
+                    form="paymentForm"
+                    disabled={paymentLoading || !paymentForm.isValid()}
+                  >
                     ACCEPT
                   </Button>
-                  <Button fullWidth color="red" disabled={paymentLoading}>
+                  <Button fullWidth color="red" disabled={paymentLoading} onClick={() => navigate("/profile")}>
                     CANCEL
                   </Button>
                 </Flex>
